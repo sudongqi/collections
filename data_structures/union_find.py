7af9a906c9eab5f08c9d50641905b253340e49e1
sdq <sdq@sdqs-air.lan>
2018-09-03 12:00:04 -0700

    update

---
+++ data_structures/union_find.py
@@ -0,0 +1,31 @@
+class UnionFind(object):
+	def __init__(self):
+		self.par = {}
+		self.rnk = {}
+
+	def __getitem__(self, x):
+		if x not in self.par:
+			self.par[x] = x
+			self.rnk[x] = 0
+		if self.par[x] != x:
+			self.par[x] = self[self.par[x]]
+		return self.par[x]
+
+	def union(self, x, y):
+		xr, yr = self[x], self[y]
+		if self.rnk[xr] < self.rnk[yr]:
+			self.par[xr] = yr
+		elif self.rnk[xr] > self.rnk[yr]:
+			self.par[yr] = xr
+		else:
+			self.par[yr] = xr
+			self.rnk[xr] += 1
+			
+uf = UnionFind()
+for i in range(10):
+	uf[i]
+uf.union(0, 9)
+uf.union(1, 8)
+uf.union(0, 1)
+print (uf[7]==uf[9])
+print (uf[8]==uf[9])
--- machine_learning/k_means.py
+++ machine_learning/k_means.py
@@ -1,8 +1,24 @@
 import numpy as np
 import random
 
-from data_utils import *
-
+def read_iris_data():
+    raw_data = []
+    labels = {}
+    label_index = 0
+    with open('./datasets/iris.data') as f:
+        for line in f:
+            if len(line)==1:
+                continue
+            data = line.split(',')
+            data[-1] = data[-1][:-1]
+            for i in range(len(data)-1):
+                data[i] = float(data[i])
+            if data[-1] not in labels:
+                labels[data[-1]] = label_index
+                label_index += 1
+            data[-1] = labels[data[-1]]
+            raw_data.append(data)
+    return raw_data, labels
 
 class k_means:
     def __init__(self, num_iterations=20, k=3):
