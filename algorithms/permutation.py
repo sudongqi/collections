2d96885947289a3470c9a9c14da9e335b1262971
sdq <sdq@sdqs-air.lan>
2018-09-03 18:16:41 -0700

    update

---
+++ algorithms/permutation.py
@@ -0,0 +1,17 @@
+def unique_permutation(arr):
+	res= [[]]
+	for n in arr:
+		new_res = []
+		for sub_arr in res:
+			for i in range(len(sub_arr) + 1):
+				# insert n into the partially completed array
+				new_res.append(sub_arr[:i] + [n] + sub_arr[i:])
+				# avoid inserting n right next to another n
+				if i < len(sub_arr) and sub_arr[i]==n:
+					break
+		res = new_res
+	return res
+	
+vec = [1, 1, 2]
+# should print: [[2, 1, 1], [1, 2, 1], [1, 1, 2]]
+print(unique_permutation(vec))
